e1cde013deb2abccbaae47ea8c2157746779b487
samarth <samarth@smalldaytech.com>
2020-09-25 04:50:20 +0530

    updated settings

--- gw_crawler/malicious_file_crawler/src/settings.py
+++ gw_crawler/malicious_file_crawler/src/settings.py
@@ -8,17 +8,30 @@
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import os
 
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 from src.constants import zip_download_path
-from dotenv import load_dotenv
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
+from dotenv import load_dotenv
 env_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 load_dotenv()
 
 PROJECT_NAME = 'malicious_file_crawler'
 # Define JOBDIR path for pausing and resuming crawls
-JOBDIR = 'crawls/spiders'
+JOB_DIR = 'crawls/glasswallspider-1'
 
+# scrapyd endpoint
+#SCRAPYD_ENDPOINT = 'http://localhost:6800'
+
+# EXTENSIONS = {
+#    'scrapy_dotpersistence.DotScrapyPersistence': 0,
+# }
+
+# DOTSCRAPY_ENABLED = True
+#
+# ADDONS_AWS_ACCESS_KEY_ID ='AWS ACCESS KEY'
+# ADDONS_AWS_SECRET_ACCESS_KEY ="AWS SECRET KEY"
+# # ADDONS_AWS_USERNAME = "username"
+# ADDONS_S3_BUCKET = "BUCKET NAME"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Get the path to the directory this file is in
@@ -42,21 +55,16 @@ ROBOTSTXT_OBEY = False
 CONCURRENT_REQUESTS = 32
 REACTOR_THREADPOOL_MAXSIZE = 20
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     "src.pipelines.MaliciousFileCrawlerPipeline": 1,
     'src.middlewares.MaliciousFileCrawlerDownloaderMiddleware': 543,
 
 }
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "src.pipelines.MaliciousFileCrawlerPipeline": 300,
+
 }
 
 DOWNLOAD_TIMEOUT = 12000
@@ -67,3 +75,23 @@ FILES_STORE = zip_download_path
 # Uncomment this when MINIO service is running
 # max download size of 5gb
 DOWNLOAD_MAXSIZE = 5368709120
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+# AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+# AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+# AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
