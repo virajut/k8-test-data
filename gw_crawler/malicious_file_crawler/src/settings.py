cbf65c36df8d64e596ef8ba63ef25ba9b7fe832a
samarth <samarth@smalldaytech.com>
2020-10-01 05:23:06 +0530

    refactor

--- gw_crawler/malicious_file_crawler/src/settings.py
+++ gw_crawler/malicious_file_crawler/src/settings.py
@@ -9,7 +9,7 @@
 import os
 
 from src.constants import DOWNLOAD_PATH
-
+from datetime import datetime
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 from dotenv import load_dotenv
 
@@ -18,21 +18,40 @@ load_dotenv()
 
 PROJECT_NAME = 'malicious_file_crawler'
 # Define JOBDIR path for pausing and resuming crawls
-JOB_DIR = 'crawls/spiders'
+#JOB_DIR = 'crawlers/spider-1'
 # scrapyd endpoint
 # SCRAPYD_ENDPOINT = 'http://localhost:6800'
 
-# EXTENSIONS = {
-#    'scrapy_dotpersistence.DotScrapyPersistence': 0,
+
+
+
+# DOWNLOADER_MIDDLEWARES = {
+#         'history.middleware.HistoryMiddleware': 901 # Right after HttpCacheMiddleware
 # }
 
-# DOTSCRAPY_ENABLED = True
-#
-# ADDONS_AWS_ACCESS_KEY_ID ='AWS ACCESS KEY'
-# ADDONS_AWS_SECRET_ACCESS_KEY ="AWS SECRET KEY"
-# # ADDONS_AWS_USERNAME = "username"
-# ADDONS_S3_BUCKET = "BUCKET NAME"
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#EPOCH = True
+# HISTORY = {
+#     'STORE_IF'   : 'history.logic.StoreAlways',
+#     'RETRIEVE_IF': 'history.logic.RetrieveAlways',
+#     'BACKEND'    : 'history.storage.S3CacheStorage',
+#     'S3_ACCESS_KEY': 'AKIAIULADVGM55FLEA7A',
+#     'S3_SECRET_KEY': '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',
+#     'S3_BUCKET'    : 'abcd',
+#     'USE_PROXY'  : True,
+# }
+
+HTTPCACHE_ENABLED = True
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+date = datetime.strftime(datetime.now(), '%Y%m%d')
+HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 24 * 7
+HTTPCACHE_DIR = 'httpcache'
+#S3CACHE_URI='s3://AKIAIULADVGM55FLEA7A:lB2RehGmcyKaSLBLrEa7IWd6KF0Pxr5F9rYrvkCf@bucket/name'
+
+EXTENSIONS = {
+   'scrapy_dotpersistence.DotScrapyPersistence': 0,
+}
+
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Get the path to the directory this file is in
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -88,16 +107,13 @@ DOWNLOADER_MIDDLEWARES = {
     'src.middlewares.MaliciousFileCrawlerDownloaderMiddleware': 543,
 
 }
-
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "src.pipelines.MaliciousFileCrawlerPipeline": 300,
-
 }
 
 DOWNLOAD_TIMEOUT = 12000
@@ -120,11 +136,3 @@ DOWNLOAD_MAXSIZE = 5368709120
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 # AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
