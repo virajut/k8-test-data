31c88faeabb0fbaacf1ff2cc429c749004b0c193
Viraj <virajthakrar@gmail.com>
2020-09-30 19:36:03 +0530

    Moved requirements and main file out of src

--- to_s3/src/config.py
+++ to_s3/src/config.py
@@ -13,13 +13,13 @@ class Config(object):
     download_path = "/usr/src/app/minio_files/"
 
     # Set S3 credentials
-    S3_ENDPOINT = get_envar('S3_ENDPOINT', required=True)
-    S3_ACCESS_KEY_ID = get_envar('S3_ACCESS_KEY_ID', required=True)
-    S3_SECRET_ACCESS_KEY = get_envar('S3_SECRET_ACCESS_KEY', required=True)
-    S3_REGION = get_envar('S3_REGION', required=False)
+    S3_ENDPOINT = get_envar("S3_ENDPOINT", required=True)
+    S3_ACCESS_KEY_ID = get_envar("S3_ACCESS_KEY_ID", required=True)
+    S3_SECRET_ACCESS_KEY = get_envar("S3_SECRET_ACCESS_KEY", required=True)
+    S3_REGION = get_envar("S3_REGION", required=False)
     
     # Set Minio Credentials
-    MINIO_ENDPOINT = get_envar('MINIO_ENDPOINT', required=True)
-    MINIO_ACCESS_KEY_ID = get_envar('MINIO_ACCESS_KEY_ID', required=True)
-    MINIO_SECRET_ACCESS_KEY = get_envar('MINIO_SECRET_ACCESS_KEY', required=True)
-    MINIO_SECURE = get_envar('MINIO_SECURE', required=False)
+    MINIO_ENDPOINT = get_envar("MINIO_ENDPOINT", required=True)
+    MINIO_ACCESS_KEY_ID = get_envar("MINIO_ACCESS_KEY_ID", required=True)
+    MINIO_SECRET_ACCESS_KEY = get_envar("MINIO_SECRET_ACCESS_KEY", required=True)
+    MINIO_SECURE = get_envar("MINIO_SECURE", required=False)
