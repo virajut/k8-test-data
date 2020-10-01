352aa82e379efa9706e5761c97230d2055cac1e5
samarth <samarth@smalldaytech.com>
2020-10-01 05:54:07 +0530

    refactor

--- gw_crawler/malicious_file_crawler/src/settings.py
+++ gw_crawler/malicious_file_crawler/src/settings.py
@@ -9,7 +9,7 @@
 import os
 
 from src.constants import DOWNLOAD_PATH
-from datetime import datetime
+
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 from dotenv import load_dotenv
 
@@ -22,27 +22,11 @@ PROJECT_NAME = 'malicious_file_crawler'
 # scrapyd endpoint
 # SCRAPYD_ENDPOINT = 'http://localhost:6800'
 
-
-
-
-# DOWNLOADER_MIDDLEWARES = {
-#         'history.middleware.HistoryMiddleware': 901 # Right after HttpCacheMiddleware
-# }
-
-#EPOCH = True
-# HISTORY = {
-#     'STORE_IF'   : 'history.logic.StoreAlways',
-#     'RETRIEVE_IF': 'history.logic.RetrieveAlways',
-#     'BACKEND'    : 'history.storage.S3CacheStorage',
-#     'S3_ACCESS_KEY': 'AKIAIULADVGM55FLEA7A',
-#     'S3_SECRET_KEY': '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',
-#     'S3_BUCKET'    : 'abcd',
-#     'USE_PROXY'  : True,
-# }
-
+from datetime import datetime
 HTTPCACHE_ENABLED = True
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 date = datetime.strftime(datetime.now(), '%Y%m%d')
+
 HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 24 * 7
 HTTPCACHE_DIR = 'httpcache'
 #S3CACHE_URI='s3://AKIAIULADVGM55FLEA7A:lB2RehGmcyKaSLBLrEa7IWd6KF0Pxr5F9rYrvkCf@bucket/name'
@@ -51,8 +35,7 @@ EXTENSIONS = {
    'scrapy_dotpersistence.DotScrapyPersistence': 0,
 }
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Get the path to the directory this file is in
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -107,8 +90,10 @@ DOWNLOADER_MIDDLEWARES = {
     'src.middlewares.MaliciousFileCrawlerDownloaderMiddleware': 543,
 
 }
+
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
+
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
@@ -136,3 +121,11 @@ DOWNLOAD_MAXSIZE = 5368709120
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 # AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
