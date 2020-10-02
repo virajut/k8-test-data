63220cd0ad5da471851529a3650467e672c64e33
samarth <samarth@smalldaytech.com>
2020-10-02 18:39:54 +0530

    updated settings

--- gw_crawler/malicious_file_crawler/src/settings.py
+++ gw_crawler/malicious_file_crawler/src/settings.py
@@ -12,15 +12,12 @@ from src.constants import DOWNLOAD_PATH
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 from dotenv import load_dotenv
-
 env_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 load_dotenv()
 
 PROJECT_NAME = 'malicious_file_crawler'
 # Define JOBDIR path for pausing and resuming crawls
 #JOB_DIR = 'crawlers/spider-1'
-# scrapyd endpoint
-# SCRAPYD_ENDPOINT = 'http://localhost:6800'
 
 from datetime import datetime
 HTTPCACHE_ENABLED = True
@@ -29,7 +26,6 @@ date = datetime.strftime(datetime.now(), '%Y%m%d')
 
 HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 24 * 7
 HTTPCACHE_DIR = 'httpcache'
-#S3CACHE_URI='s3://AKIAIULADVGM55FLEA7A:lB2RehGmcyKaSLBLrEa7IWd6KF0Pxr5F9rYrvkCf@bucket/name'
 
 EXTENSIONS = {
    'scrapy_dotpersistence.DotScrapyPersistence': 0,
@@ -68,21 +64,6 @@ REACTOR_THREADPOOL_MAXSIZE = 20
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-# }
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    'src.middlewares.MaliciousFileCrawlerSpiderMiddleware': 543,
-# }
-
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
@@ -90,10 +71,6 @@ DOWNLOADER_MIDDLEWARES = {
     'src.middlewares.MaliciousFileCrawlerDownloaderMiddleware': 543,
 
 }
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
@@ -109,23 +86,3 @@ FILES_STORE = DOWNLOAD_PATH
 # Uncomment this when MINIO service is running
 # max download size of 5gb
 DOWNLOAD_MAXSIZE = 5368709120
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
