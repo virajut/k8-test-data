b9a25ec38701695cc36e94448347dec3c1790a4b
samarth <samarth@smalldaytech.com>
2020-10-13 18:07:46 +0530

    fixed s3 sync path issue

--- file_processor/src/mq_service.py
+++ file_processor/src/mq_service.py
@@ -1,11 +1,21 @@
+import json
+import logging as logger
 import os
 
 import requests
 
+logger.basicConfig(level=logger.INFO)
 class MQService:
 
     @staticmethod
     def send(payload):
         s3_sync_api=os.environ.get('s3_sync_api')
+        logger.info(f's3_sync_api : {s3_sync_api}')
+
+        payload=json.dumps(payload)
+        payload=json.loads(payload)
+
+        logger.info(f'MQService: send : payload : {payload}')
+
         response = requests.post(s3_sync_api, json=payload)
 
