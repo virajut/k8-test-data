3d461ed2a54d7e88a25109623706ec78de3497d9
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2020-10-13 21:31:02 +0545

    s3 upload to each folder with type

--- s3_sync/src/__init__.py
+++ s3_sync/src/__init__.py
@@ -41,7 +41,13 @@ def create_app():
         s3_client = S3Client(Config.S3_URL, Config.S3_ACCESS_KEY, Config.S3_SECRET_KEY)
         try:
             # bucket_name=file_to_fetch['s3_bucket'], # use later
-            s3_client.upload_file(file_path=file_from_minio, file_name=file_to_fetch['file'])
+            bucket = "other"
+            try:
+                bucket = file_to_fetch['file'].split(".")[-1]
+            except Exception:
+                pass
+                
+            s3_client.upload_file(file_path=file_from_minio, file_name=file_to_fetch['file'], bucket=bucket)
         except Exception as err:
             logger.error(f'create_app: s3_client {err}')
             return validation_error(str(err))
