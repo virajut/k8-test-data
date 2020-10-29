bfcf025dbb0f629b6e0829a477f8a0766c51f8eb
Viraj <virajthakrar@gmail.com>
2020-10-29 17:09:24 +0530

    Adding security checks to k8 test data project

--- rabbitmq/src/config.py
+++ rabbitmq/src/config.py
@@ -3,8 +3,8 @@ import os
 
 def get_envar(k, required=True):
     val = os.environ.get(k, None)
-    if not val and required:
-        exit(f"{k} not supplied")
+    # if not val and required:
+    #     exit(f"{k} not supplied")
     return val
 
 class Config(object):
